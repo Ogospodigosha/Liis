0a7917070e48451ed08266df353d726cb2c77afa
Ogospodigosha <odintsovis@hotmail.com>
2023-04-01 20:42:27 +0300

    add: Button

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './button.module.css'
+
+type PropsType = {
+    title: string
+    size: string
+}
+
+export const Button:React.FC<PropsType> = ({title, size}) => {
+    return (
+       <button  className={s.button} style={{width: size}}>{title}</button>
+    );
+};
