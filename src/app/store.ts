bfbe739256a63f14f3c7ef67789b8504a2097f47
Ogospodigosha <odintsovis@hotmail.com>
2023-04-03 14:11:41 +0300

    fix: rootWatcher

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,16 +2,21 @@ import { TypedUseSelectorHook, useSelector } from "react-redux";
 import {applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import createSagaMiddleware from 'redux-saga'
 import {authReducer, authWatcher} from "../features/auth/authReducer";
+import {searchHotelReducer,  searchWatcher} from "../features/searchHotel/SearchHotelReducer";
+import {all} from "redux-saga/effects";
 
 
 export const useAppSelector:TypedUseSelectorHook<AppRootState> = useSelector
 const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    searchHotel: searchHotelReducer
 })
 const sagaMiddleware = createSagaMiddleware()
 export const store = legacy_createStore(rootReducer, applyMiddleware(sagaMiddleware))
 export type AppRootState = ReturnType<typeof rootReducer>
 sagaMiddleware.run(rootWatcher)
 function* rootWatcher(){
-   yield authWatcher()
+    debugger
+    yield all([authWatcher(),searchWatcher() ])
+
 }
