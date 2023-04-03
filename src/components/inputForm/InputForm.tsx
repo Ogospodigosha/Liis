135b5d8d82a60425a44f0318d231829628edc584
Ogospodigosha <odintsovis@hotmail.com>
2023-04-03 14:04:44 +0300

    fix: InputForm

--- src/components/inputForm/InputForm.tsx
+++ src/components/inputForm/InputForm.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { useField } from 'formik';
 import React from 'react';
 import s from './inputForm.module.css'
@@ -7,16 +8,18 @@ type PropsType = {
     name: string
     type: string
     label: string
+    size?: string
 }
-export const InputForm:React.FC<PropsType> = ({name, type, label}) => {
+export const InputForm:React.FC<PropsType> = ({name, type, label, size}) => {
     const [field, meta] = useField(name)
     const errorColor = {
-        color:meta.touched && meta.error ? '#EB1717': ''
+        color:meta.touched && meta.error ? '#EB1717': '',
+        width:size
     }
     return (
         <>
-            <div className={ s.inputLabel} style={errorColor}>{label}</div>
-            <input {...field} style={errorColor}  type={type} />
+            <div className={ s.inputLabel} style={errorColor} >{label}</div>
+            <input {...field} style={errorColor}  type={type} min={type === 'date' ? dayjs().format('YYYY-MM-DD'): ''} />
             {meta.touched && meta.error ? <div className={s.error}>{meta.error}</div> : null}
         </>
     );
