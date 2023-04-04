be0fa4a989aaf3316e2a6ade719f0f99fd14f749
Ogospodigosha <odintsovis@hotmail.com>
2023-04-05 01:21:13 +0300

    add: class for hotelsRender

--- src/features/searchHotel/hotelsDisplay/hotelsRenderList/HotelsRenderList.tsx
+++ src/features/searchHotel/hotelsDisplay/hotelsRenderList/HotelsRenderList.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {useAppSelector} from "../../../../app/store";
 import HotelItem from "./hotelItem/HotelItem";
 import {v4} from "uuid";
-
+import s from './hotelsRenderList.module.css'
 export const HotelsRenderList = () => {
     const hotels = useAppSelector(state => state.searchHotel.hotels)
     const favoriteHotels = useAppSelector(state => state.searchHotel.favoriteHotels)
       return (
-        <div style={{overflowY: 'auto', height: '460px', overflowX: 'hidden', whiteSpace: 'nowrap'}}>
-            <div style={{marginBottom: '12px'}}>Добавлено в Избранное: {favoriteHotels.length} отелей</div>
-            <div style={{width: '550px'}}>
+        <div className={s.forHeight}>
+            <div className={s.margin}>Добавлено в Избранное: {favoriteHotels.length} отелей</div>
+            <div className={s.width}>
                 {hotels.map((el,index) =>
                     <HotelItem key={v4()} el={el} noImage={true}/>
                 )}
